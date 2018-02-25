c4f5f0c39f7ae21b3734562ecd76ba232551ee74
saufisyafiq <36787191+saufisyafiq@users.noreply.github.com>
2018-02-25 11:29:22 +0800

    Update MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -7,6 +7,7 @@ class MyThread extends Thread {
     public void run() {
         System.out.println("running...");
         System.out.println("running....new 1");
+        System.out.println("SAUFI");
     }
 
     public static void main(String args[]) {
